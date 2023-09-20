678f459499d5ebe83013defcb6f6ca9ee048244b
franklinsk <franklinsk@hotmail.com>
2023-09-20 13:12:14 -0500

    adding api

---
+++ CalculatorApi/Controllers/CalculatorController.cs
@@ -0,0 +1,69 @@
+﻿using CalculatorApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCalculator")]
+        public async Task<IActionResult> Get(int? input1, int? input2, int? size)
+        {
+            if (!input1.HasValue || !input2.HasValue || !size.HasValue)
+            {
+                return BadRequest("Input parameters are missing.");
+            }
+            if (input1 == 0 || input2 == 0)
+            {
+                return BadRequest("Dividers can't be zero.");
+            }
+            if (size < 0)
+            {
+                return BadRequest("Size should be positive.");
+            }
+
+            List<CalculationResult> results = new List<CalculationResult>();
+
+
+            for (int number = 1; number <= size; number++)
+            {
+                string result = "N/A";
+
+                if (number % input1 == 0)
+                {
+                    result = "yes";
+                }
+
+                if (number % input2 == 0)
+                {
+                    if (result == "yes")
+                    {
+                        result = "I dont know";
+                    }
+                    else
+                    {
+                        result = "no";
+                    }
+                }
+
+                results.Add(new CalculationResult
+                {
+                    Number = number,
+                    Result = result
+                });
+            }
+
+            return   Ok(results);
+        }
+    }
+}
--- QualiFactsCodeChallenge/Controllers/HomeController.cs
+++ QualiFactsCodeChallenge/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using QualiFactsCodeChallenge.Models;
 using System.Diagnostics;
+using System.Net;
+using System.Text.Json.Serialization;
 
 //QUALIFACTS CODE CHALLENGE
 namespace QualiFactsCodeChallenge.Controllers
@@ -9,9 +12,14 @@ namespace QualiFactsCodeChallenge.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        Uri baseAddress = new Uri("https://localhost:7264/api/Calculator");
+        private readonly HttpClient _client;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _client= new HttpClient(); 
+            _client.BaseAddress = baseAddress;  
         }
 
         public IActionResult Index()
@@ -56,6 +64,21 @@ namespace QualiFactsCodeChallenge.Controllers
             return Json(results);
         }
 
+        [HttpPost]
+        public JsonResult CalculateApi(int input1, int input2, int size)
+        {
+            List<CalculationResult> results = new List<CalculationResult>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + String.Format("?input1={0}&input2={1}&size={2}", input1, input2,size)).Result;
+            String data = "";
+
+            if (response.IsSuccessStatusCode)
+            {
+                data = response.Content.ReadAsStringAsync().Result;
+                results = JsonConvert.DeserializeObject<List<CalculationResult>>(data);
+            }
+
+            return Json(results);
+        }
         public IActionResult Privacy()
         {
             return View();
